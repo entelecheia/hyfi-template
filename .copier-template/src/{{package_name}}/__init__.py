c7451ccd0ded5ac82109baa8d4f47549ba0edd76
Young Joon Lee <entelecheia@hotmail.com>
2023-04-20 09:29:52 +0000

    fix: get init info from pyproject

--- .copier-template/src/{{package_name}}/__init__.py
+++ .copier-template/src/{{package_name}}/__init__.py
@@ -1,14 +1,25 @@
+import os
+import toml
 from hyfi import about, global_config
 
 from ._version import __version__
 
-about.name = "ekorpkit"
-about.author = "Young Joon Lee"
-about.description = "ekorpkit provides a flexible interface for NLP and ML research pipelines \
-such as extraction, transformation, tokenization, training, and visualization."
-about.homepage = "https://ekorpkit.entelecheia.ai"
+# Read and parse pyproject.toml
+current_dir = os.path.dirname(os.path.abspath(__file__))
+root_dir = os.path.dirname(current_dir)
+pyproject_path = os.path.join(root_dir, "pyproject.toml")
+
+with open(pyproject_path) as f:
+    pyproject_data = toml.load(f)
+
+# Extract package information from pyproject.toml
+tool_data = pyproject_data.get("tool", {}).get("poetry", {})
+about.name = tool_data.get("name", "package name")
+about.author = tool_data.get("authors", ["Author name"])[0]
+about.description = tool_data.get("description", "package description")
+about.homepage = tool_data.get("homepage", "https://package.homepage")
 about.version = __version__
-global_config.hyfi_package_config_path = "pkg://ekorpkit.conf"
+global_config.hyfi_package_config_path = "pkg://{}.conf".format(about.name)
 
 
 def get_version() -> str:
