92f38a905fcc634bf48eaa4299b62c879d5702ba
Young Joon Lee <entelecheia@hotmail.com>
2023-04-20 10:56:04 +0000

    chore: update code template

--- src/hyfi_template/__init__.py
+++ src/hyfi_template/__init__.py
@@ -7,18 +7,17 @@ from ._version import __version__
 
 # Read and parse pyproject.toml
 current_dir = os.path.dirname(os.path.abspath(__file__))
-root_dir = os.path.dirname(os.path.dirname(current_dir))
-pyproject_path = os.path.join(root_dir, "pyproject.toml")
+pyproject_path = os.path.join(current_dir, "project.toml")
 
 with open(pyproject_path) as f:
     pyproject_data = toml.load(f)
 
 # Extract package information from pyproject.toml
-tool_data = pyproject_data.get("tool", {}).get("poetry", {})
-about.name = tool_data.get("name", "package name")
-about.author = tool_data.get("authors", ["Author name"])[0]
-about.description = tool_data.get("description", "package description")
-about.homepage = tool_data.get("homepage", "https://package.homepage")
+project_data = pyproject_data.get("metadata", {})
+about.name = project_data.get("name", "package name")
+about.author = project_data.get("authors", ["Author name"])[0]
+about.description = project_data.get("description", "package description")
+about.homepage = project_data.get("homepage", "https://package.homepage")
 about.version = __version__
 global_config.hyfi_package_config_path = "pkg://hyfi_template.conf"
 
