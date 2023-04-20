bec4d6a60227c93a931fe17ae945bdf4475b91f4
Young Joon Lee <entelecheia@hotmail.com>
2023-04-20 09:38:01 +0000

    fix: get init info from pyproject

--- src/hyfi_template/__cli__.py
+++ src/hyfi_template/__cli__.py
@@ -1,8 +1,12 @@
-"""Command line interface for {{package_name}}"""
+"""Command line interface for hyfi_template"""
 
 # Importing the libraries
 from hyfi import hydra_main
 
 
-if __name__ == "__main__":
+def main() -> None:
     hydra_main()
+
+
+if __name__ == "__main__":
+    main()
