e3c1f77cac3cdedd433a8e3c9a6e1c59e7b84a57
Young Joon Lee <entelecheia@hotmail.com>
2023-07-22 21:20:29 +0900

    refactor(package): update initialization, simplify imports, and set version in package information

--- .copier-template/src/{{package_name}}/__init__.py
+++ .copier-template/src/{{package_name}}/__init__.py
@@ -1,14 +1,15 @@
 import os
 
-from hyfi import HyFI, about, global_config  # type: ignore
+from hyfi import HyFI, about
 
-from ._version import __version__  # type: ignore
+from ._version import __version__
 
 # Read the package name from the current directory
 __package_name__ = os.path.basename(os.path.dirname(__file__))
 
-# Extract package information
+# Set package information in about
 about.__package_name__ = __package_name__
+about.__version__ = __version__
 
 # Initialize the logger
 HyFI.setLogger()
